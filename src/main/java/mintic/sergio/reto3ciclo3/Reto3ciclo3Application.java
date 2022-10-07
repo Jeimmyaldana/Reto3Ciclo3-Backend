2bbae75207e203fa14465e62d987b78286b3e507
Sofía <sofia.neira@ias.com.com>
2022-10-06 21:23:04 -0500

    cors config

--- src/main/java/mintic/sergio/reto3ciclo3/Reto3ciclo3Application.java
+++ src/main/java/mintic/sergio/reto3ciclo3/Reto3ciclo3Application.java
@@ -2,8 +2,9 @@ package mintic.sergio.reto3ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class Reto3ciclo3Application {
 
 	public static void main(String[] args) {
