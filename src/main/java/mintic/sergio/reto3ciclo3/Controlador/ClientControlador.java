154e879c9b00047c22b3b0683a5185c6a04c1c0c
Sofía <sofia.neira@ias.com.com>
2022-10-06 21:13:39 -0500

    cross origin add line

--- src/main/java/mintic/sergio/reto3ciclo3/Controlador/ClientControlador.java
+++ src/main/java/mintic/sergio/reto3ciclo3/Controlador/ClientControlador.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Client")
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ClientControlador {
          @Autowired
     private ClientServicio clientService;
